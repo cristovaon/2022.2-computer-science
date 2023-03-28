7dfde1881f7aff8d9d18f12f4ee842301746dcc6
121110199 CRISTOVAO PESSOA CANDIDO NETO <cristovao.candido.neto@lcc3-084.lcc.ufcg.edu.br>
2023-03-28 11:17:56 -0300

    LOAC: Add R01

---
+++ LOAC/R01/CristovaoPessoaCandidoNeto_Roteiro01/elevador.sv
@@ -0,0 +1,13 @@
+// Problema 01 - Elevador
+// Cristovão Pessoa Cândido Neto - 121110199
+module elevator(
+    input logic A1, A2, A3, M,
+    output logic P
+);
+  
+  	// podemos escrever a expressão lógica quando já sabemos ela
+  	
+  always_comb P <= (A1 | A2 | A3) & ~M;
+  
+  
+endmodule
