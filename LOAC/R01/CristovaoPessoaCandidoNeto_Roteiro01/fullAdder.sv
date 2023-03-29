2ef4d64231b4644939ae20d06759f810ac9d11d0
Crisnzx <cristovaon13@gmail.com>
2023-03-28 21:11:47 -0300

    LOAC: Add R01

---
+++ LOAC/R01/CristovaoPessoaCandidoNeto_Roteiro01/fullAdder.sv
@@ -0,0 +1,12 @@
+// Write your modules here!
+parameter NUM_OF_BITS=8
+module fullAdder(
+  input logic signed [NUM_OF_BITS-1:0] number1, number2
+  output logic [NUM_OF_BITS-1:0] S);
+  
+  always_comb begin
+  S = A+B
+  end
+  
+  
+endmodule
