de187f773b4f9c6da3b6b4d8113a67a4dfeb430d
Crisnzx <cristovaon13@gmail.com>
2023-04-18 18:58:32 -0300

    PLP: fix code

--- PLP/functional-paradigm/studies/labs/imposto-renda.hs
+++ PLP/functional-paradigm/studies/labs/imposto-renda.hs
@@ -1,20 +1,18 @@
 import Text.Printf
+
 main:: IO()
 main = do
-  salario <- readLn :: IO Float
-  let impostoPago = calcularImposto salario
-  let text1 = "O valor do imposto de renda retido eh: R$"
-  let text2 = "O salario liquido eh: R$"
-  printf "%s %.2f\n" text1 impostoPago
-  printf "%s $%.2f\n" text2 (salario - impostoPago)
-
+    salario <- readLn :: IO Float
+    let impostoPago = calcularImposto salario
+    let text1 = "O valor do imposto de renda retido eh: R$"
+    let text2 = "O salario liquido eh: R$"
+    printf "%s %.2f\n" text1 impostoPago
+    printf "%s $%.2f\n" text2 (salario - impostoPago)
 
 calcularImposto:: Float -> Float
 calcularImposto salario
-  | salario <= 1903.98 = calcularImpostoPorSalario salario 142.80 0.075 1903.99
-  | salario <= 3751.05 = calcularImpostoPorSalario salario 354.80 0.15 2826.66
-  | salario <= 4664.68 = calcularImpostoPorSalario salario 636.13 0.225 3751.06
-  | otherwise = calcularImpostoPorSalario salario 869.36 0.275 4664.68
-
-calcularImpostoPorSalario:: Float -> Float -> Float -> Float -> Float
-calcularImpostoPorSalario salario valorFixo porcentagem faixaTributacao = valorFixo + (salario - faixaTributacao) * porcentagem
+    | salario <= 1903.98 = 0
+    | salario <= 2826.65 = 142.80 + (salario - 1903.99) * 0.075
+    | salario <= 3751.05 = 354.80 + (salario - 2826.66) * 0.15
+    | salario <= 4664.68 = 636.13 + (salario - 3751.06) * 0.225
+    | otherwise = 869.36 + (salario - 4664.68) * 0.275
