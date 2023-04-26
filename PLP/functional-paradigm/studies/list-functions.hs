58ee52c992631fd2ef67d0630688bc85951e020e
Crisnzx <cristovaon13@gmail.com>
2023-04-26 11:56:35 -0300

    PLP: Add more own implementations of native haskell list functions

--- PLP/functional-paradigm/studies/list-functions.hs
+++ PLP/functional-paradigm/studies/list-functions.hs
@@ -6,24 +6,41 @@ getByIndex (h:t) 0 = h
 getByIndex (h:t) index = getByIndex t (index-1)
 
 -- head exampleList = 1
-getHead:: [t] -> t
-getHead (h:t) = h
+myHead:: [t] -> t
+myHead (h:t) = h
 
 -- tail exampleList = [2,3,4]
-getTail:: [t] -> [t]
-getTail (h: t) = t
+myTail:: [t] -> [t]
+myTail (h: t) = t
 
 -- last exampleList = 4
-getLast:: [t] -> t
-getLast (h: []) = h
-getLast (h: t) = getLast t
+myLast:: [t] -> t
+myLast (h: []) = h
+myLast (h: t) = myLast t
 
 -- length exampleList = 4
-getLength:: [t] -> Int
-getLength [] = 0
-getLength (h: t) = 1 + getLength t
+myLength:: [t] -> Int
+myLength [] = 0
+myLength (h: t) = 1 + myLength t
 
-contains:: [Int] -> Int -> Bool
-contains [] t = False
-contains (h: t) element = if element == h then True
-  else contains t element
+-- 4 `elem` exampleList = True
+myElem:: Eq t => [t] -> t -> Bool
+myElem [] t = False
+myElem (h: t) element = if element == h then True
+  else myElem t element
+
+-- reverse exampleList = [4,3,2,1]
+myReverse:: [t] -> [t]
+myReverse [] = []
+myReverse (h: t) = myReverse t ++ [h]
+
+-- take 3 exampleList = [1,2,3]
+myTake:: Int -> [t] -> [t]
+myTake 0 l = []
+myTake i (h:t) = [h] ++ myTake (i-1) t
+
+-- drop 3 exampleList = [4]
+myDrop:: Int -> [t] -> [t]
+myDrop 0 l = l
+myDrop i [] = []
+myDrop i (h:t) = myDrop (i-1) t
